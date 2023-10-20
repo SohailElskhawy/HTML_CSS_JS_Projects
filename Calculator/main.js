56d61a5eb7985291c7d6d8cfb8e20f74eaf70f17
SohailElskhawy <sohailelskhawy2@gmail.com>
2023-10-20 09:56:51 +0300

    Updated calculator project to fix a bug

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,44 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const allButtons = document.querySelectorAll('.number');
+    allButtons.forEach(but =>{
+        but.setAttribute('onclick', `writeNumber('${but.innerHTML}')`)
+    })
+})
+
+
 function writeNumber(num){
-    const place1El = document.querySelector('.place1');
-    if (num === '.') {
-        if (place1El.innerHTML.length === 0){
-            place1El.innerHTML += '0' + num;
-        }else if (!place1El.innerHTML.includes('.')) {
-            place1El.innerHTML += num;
-        }
+    const place1El = document.querySelector('.writingPlace');
+    const operationList = ['+', '-', '*', '/', '.'];
+    if (operationList.includes(num) && place1El.innerHTML.length == 0) {
+        return false;
+    }else{
+        place1El.innerHTML += num;
     }
-    else if (place1El.innerHTML.length < 15){
-        place1El.innerHTML += num
-    }
+
 }
 
 function clearPlace(){
-    const place1El = document.querySelector('.place1');
-    const place2El = document.querySelector('.place2');
-    place1El.innerHTML = '';
-    place2El.innerHTML = '';
+    const writingPlaceEl = document.querySelector('.writingPlace');
+    writingPlaceEl.innerHTML = ''
 }
 
-function writeQuestion(sym){
-    const place1El = document.querySelector('.place1');
-    const place2El = document.querySelector('.place2');
-    if (place1El.innerHTML.length >= 1 && place1El.innerHTML !== '.'){
-        place2El.innerHTML += place1El.innerHTML + sym;
-        place1El.innerHTML = ''
-    }
 
-}
 
 function writeAnswer(){
-    const place1El = document.querySelector('.place1');
-    const place2El = document.querySelector('.place2');
-    const equation = place2El.innerHTML + place1El.innerHTML;
-    const answer =  equation.replace('x', '*');
-    place2El.innerHTML = '';
-    place1El.innerHTML = eval(answer);
+    const writingPlaceEl = document.querySelector('.writingPlace');
+
+    const answer =  writingPlaceEl.innerHTML
+   
+    writingPlaceEl.innerHTML = eval(answer);
 }
 
-function ceButton() {
-    const place2El = document.querySelector('.place2');
-    place2El.innerHTML = '';
+function deButton() {
+    const writingPlaceEl = document.querySelector('.writingPlace');
+    const currentText = writingPlaceEl.innerHTML;
+    if(currentText.length > 0){
+        writingPlaceEl.innerHTML = currentText.slice(0,-1);   
+    }
 }
